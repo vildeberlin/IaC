957797dcc880aac882b24f8e9ad186faced9c7b8
vildebk <vildebk@stud.ntnu.no>
2026-09-24 21:09:17 +0200

    volum

--- oblig1/Persistent_storage/main.tf
+++ oblig1/Persistent_storage/main.tf
@@ -6,3 +6,9 @@ resource "openstack_blockstorage_volume_v3" "vol" {
 output "volume_id" {
   value = openstack_blockstorage_volume_v3.vol.id
 }
+
+#kobler volumet med vm-en 
+resource "openstack_compute_volume_attach_v2" "attach" {
+  instance_id = openstack_compute_instance_v2.vm.id
+  volume_id = openstack_blockstorage_volume_v3.vol.id
+}
---
+++ oblig1/Persistent_storage/variables.tf
@@ -0,0 +1,7 @@
+variable "name" {
+  description = "Navnet på min persistant storage"
+}
+
+variable "size_gb" {
+  description = "Størrelsen på persistant storage"
+}
