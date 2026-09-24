2cd5c69031d54b52db5d732c3e00c0ac065a17bd
vildebk <vildebk@stud.ntnu.no>
2026-09-24 20:56:21 +0200

    outputs

---
+++ oblig1/Virtual_machine/outputs.tf
@@ -0,0 +1,7 @@
+output "openstack_compute_instance_v2" {
+  value = openstack_compute_instance_v2.vm.id
+}
+
+output "openstack_compute_instance_v2" {
+  value = openstack_compute_instance_v2.vm.IPv4
+}
