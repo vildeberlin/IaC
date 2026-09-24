9f025081766675faf1d8a12b8e9cd2af33959a52
vildebk <vildebk@stud.ntnu.no>
2026-09-24 16:20:35 +0200

    backend

---
+++ oblig1/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.12.0"
+
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 3"
+    }
+  }
+
+  backend "kubernetes" {
+    secret_suffix = "lab04"
+    namespace     = "iac"
+    config_path   = "./kube/config"
+
+  }
+}
+
+
+
+# Configure the OpenStack Provider
+provider "openstack" {
+}
