79507d69ae9cc90f683c97054cb0d5c6a9439e2b
Alida <alidaskaretkrogstad@gmail.com>
2026-09-24 16:08:33 +0200

    skriver ut subnet id

--- oblig1/Network/output.tf
+++ oblig1/Network/output.tf
@@ -1,3 +1,9 @@
+// skriver ut Subnet ID
+output "subnet_id" {
+  value = openstack_networking_subnet_v2.subnet.id
+}
+
+/*
 output "network_name" {
   value = openstack_networking_network_v2.network.name
 }
@@ -8,3 +14,4 @@ output "subnet_name" {
 output "router_id" {
   value = openstack_networking_router_v2.network.id
 }
+*/
