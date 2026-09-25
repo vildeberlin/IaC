37070ae4ce796804b7b100040b6078c6f46f0900
Alida <alidaskaretkrogstad@gmail.com>
2026-09-25 13:00:39 +0200

    lagde en variables fil for main i root

--- oblig1/main.tf
+++ oblig1/main.tf
@@ -1,4 +1,15 @@
 module "Network" {
+    source = "./Network"
+
+    network_name          = var.network_name
+    network_subnet_name   = "${var.network_name}-subnet"
+    network_subnet_cidr   = var.subnet_cidrs
+    network_router_name   = var.router_name
+    subnet                = var.subnet_cidrs
+    ntnu_internal_network = var.external_network_name
+  }
+
+/*module "Network" {
   source = "./Network"
 
   network_name        = "network"
@@ -14,8 +25,38 @@ module "Network" {
     backend  = "10.0.2.0/24"
     database = "10.0.3.0/24"
   }
-}
+}*/
 
+  module "vm_frontend" {
+    source = "./Virtual_machine"
+
+    name           = var.frontend_vm_name
+    flavor         = var.frontend_flavor
+    distro         = var.image_name
+    ssh_key_name   = var.ssh_key_name
+    network_id     = module.Network.network_id
+    subnet_id      = module.Network.subnet_ids["frontend"]
+    template       = "${path.root}/${var.frontend_template}"
+    secgroup_name  = var.frontend_secgroup_name
+    security_rules = local.frontend_rules
+  }
+
+  module "vm_db" {
+    source = "./Virtual_machine"
+
+    name           = var.db_vm_name
+    flavor         = var.db_flavor
+    distro         = var.image_name
+    ssh_key_name   = var.ssh_key_name
+    network_id     = module.Network.network_id
+    subnet_id      = module.Network.subnet_ids["database"]
+    template       = "${path.root}/${var.db_template}"
+    secgroup_name  = var.db_secgroup_name
+    security_rules = local.db_rules
+  }
+
+
+/*
 module "vm_frontend" {
   source     = "./Virtual_machine"
 
@@ -47,21 +88,45 @@ module "vm_frontend" {
       postgres = { port = 5432, protocol = "tcp", cidr = "10.0.1.0/24" } # bare fra frontend-subnettet
     }
   }
+  */
 
 module "object_storage" {
   source = "./Storage_container"
 
-  storage_container_name         = "this"
+  storage_container_name         = var.storage_container_name
 }
 
+  module "Persistent_storage" {
+    source = "./Persistent_storage"
+
+    name                 = var.volume_name
+    size_gb              = var.volume_size_gb
+    attach_storage_to_vm = var.attach_volume
+    instance_id          = module.vm_db.vm_id
+  }
+
+/*
 module "Persistent_storage" {
   depends_on = [module.Virtual_machine]
   source = "./Persistent_storage"
 
   name = "vol"
   instance_id = module.vm_db.vm_id  
-}
+}*/
 
+  module "Load_balancer" {
+    source = "./Load_balancer"
+
+    flag                  = var.enable_load_balancer
+    lb_name               = var.lb_name
+    protocol              = var.lb_protocol
+    protocol_port         = var.lb_port
+    member_address        = module.vm_frontend.vm_ip
+    member_subnet_id      = module.Network.subnet_ids["frontend"]
+    external_network_name = var.external_network_name
+  }
+
+/*
 module "Load_balancer" {
   depends_on = [module.Network]
   source = "./Load_balancer"
@@ -69,4 +134,4 @@ module "Load_balancer" {
   lb_name = "lb"
   member_address    = module.vm_frontend.vm_ip
   member_subnet_id  = module.Network.subnet_ids["frontend"]
-}
+}*/
---
+++ oblig1/variables.tf
@@ -0,0 +1,170 @@
+// Nettverk 
+
+variable "network_name" {
+  description = "Navn på nettverket"
+  type        = string
+  default     = "oblig1-nettverk"
+}
+
+variable "router_name" {
+  description = "Navn på routeren"
+  type        = string
+  default     = "oblig1-router"
+}
+
+variable "subnet_cidr" {
+    description = "IP-område per lag i 3-lagsarkitekturen"
+    type        = map(string)
+
+    default     = {
+        frontend = "10.0.1.0/24"
+        backend  = "10.0.2.0/24"
+        database = "10.0.3.0/24"
+    }
+}
+
+variable "external_network_name" {
+    description = "Navn på det eksterne nettverket i SkyHiGh"
+    type        = string
+    default     = "ntnu-internal"
+}
+
+
+// VM 
+
+variable "ssh_key_name" {
+  description = "Navn på SSH-nøkkel som skal brukes for å logge inn på VM"
+  type        = string
+}
+
+  variable "image_name" {
+    description = "Image som brukes på VM-ene"
+    type        = string
+    default     = "Debian 13 (Trixie) stable amd64"
+  }
+
+variable "frontend_vm_name" {
+    description = "Navn på frontend-VM-en"
+    type        = string
+    default     = "frontend"
+}
+
+  variable "frontend_flavor" {
+    description = "Flavor (størrelse) på frontend-VM-en"
+    type        = string
+    default     = "gx1.1c1r"
+  }
+
+
+variable "frontend_template" {
+    description = "Cloud-init-mal for frontend (relativ til rotmodulen)"
+    type        = string
+    default     = "templates/frontend-init.yaml"
+}
+
+variable "db_vm_name" {
+    description = "Navn på database-VM-en"
+    type        = string
+    default     = "db"
+}
+
+variable "db_flavor" {
+    description = "Flavor (størrelse) på database-VM-en"
+    type        = string
+    default     = "gx1.1c1r"
+}   
+
+
+variable "db_template" {
+    description = "Cloud-init-mal for database (relativ til rotmodulen)"
+    type        = string
+    default     = "templates/db-init.yaml"
+}
+
+
+// sikkerhetsgrupper
+
+
+variable "frontend_secgroup_name" {
+    description = "Navn på sikkerhetsgruppen til frontend"
+    type        = string
+    default     = "frontend-secgroup"
+}
+
+variable "db_secgroup_name" {
+    description = "Navn på sikkerhetsgruppen til database"
+    type        = string
+    default     = "db-secgroup"
+}
+
+variable "ssh_allowed_cidr" {
+    description = "Hvilke IP-er som får SSH-tilgang til VM-ene"
+    type        = string
+    default     = "0.0.0.0/0"
+}
+
+variable "frontend_public_ports" {
+    description = "Porter som åpnes mot frontend fra hele verden (HTTP/HTTPS)"
+    type        = list(number)
+    default     = [80, 443]
+}
+
+variable "db_port" {
+    description = "Porten databasen lytter på (5432 = PostgreSQL, 3306 = MySQL)"
+    type        = number
+    default     = 5432
+}
+
+// lastbalanserer 
+
+variable "enable_load_balancer" {
+    description = "Slå lastbalanserer av/på"
+    type        = bool
+    default     = true
+}
+
+variable "lb_name" {
+    description = "Navn på lastbalansereren"
+    type        = string
+    default     = "terranova-lb"
+}
+
+variable "lb_protocol" {
+    description = "Protokoll som skal balanseres (HTTP, HTTPS, TCP)"
+    type        = string
+    default     = "HTTP"
+}
+
+variable "lb_port" {
+    description = "Porten tjenesten lytter på"
+    type        = number
+}
+
+// Persistent storage
+
+variable "attach_volume" {
+    description = "Koble et persistent volum til database-VM-en"
+    type        = bool
+    default     = true
+}
+
+variable "volume_name" {
+    description = "Navn på det persistente volumet"
+    type        = string
+    default     = "db-volume"
+}
+
+variable "volume_size_gb" {
+    description = "Størrelse på volumet i GB"
+    type        = number
+    default     = 20
+}
+
+
+// Storage container    
+
+variable "storage_container_name" {
+    description = "Navn på storage containeren"
+    type        = string
+    default     = "terranova-container"
+}   
