976d5bcd9be05c82c85bc3a6f23257ff29b74d29
Alida <alidaskaretkrogstad@gmail.com>
2026-09-26 09:50:28 +0200

    kansje noen av de siste endringene tror jeg

--- oblig1/provider.tf
+++ oblig1/provider.tf
@@ -1,19 +1,29 @@
-terraform {
-  required_version = ">= 1.12.0"
+ terraform {
+    required_version = ">= 1.12.0"
 
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 3"
+    required_providers {
+      openstack = {
+        source  = "terraform-provider-openstack/openstack"
+        version = "~> 3"
+      }
     }
-  }
+ }
+ 
+ 
+backend "s3" {
+      bucket = "terranova-tfstate"
+      region = "us-east-1" # dummyverdi, kreves av backenden
 
-  backend "swift" {
-      container         = "terranova-tfstate"
-      archive_container = "terranova-tfstate-archive"
-      key               = "poc/terraform.tfstate"
-    }
+      endpoints = { s3 = "https://swift.skyhigh.iik.ntnu.no" }
+
+      skip_credentials_validation = true
+      skip_region_validation      = true
+      skip_requesting_account_id  = true
+      skip_metadata_api_check     = true
+      skip_s3_checksum            = true
+      use_path_style              = true
 }
+
 
 
 
